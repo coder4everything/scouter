c56ebe6d7fc97f98908d454881d56932f8fc26c5
yujeong <yujung917@daum.net>
2025-07-20 08:26:12 +0900

    Add support for configuring xlog scale via JVM options

--- scouter.client/src/scouter/client/xlog/XLogYAxisEnum.java
+++ scouter.client/src/scouter/client/xlog/XLogYAxisEnum.java
@@ -1,14 +1,14 @@
 package scouter.client.xlog;
 
 public enum XLogYAxisEnum {
-	
-	ELAPSED("Elapsed", "Elapsed (sec)", true, 9),
-	CPU("Cpu Time", "Cpu Time (ms)", false, 100),
+
+	ELAPSED("Elapsed", "Elapsed (sec)", true, System.getProperty("elapsedScale")==null?9:Integer.parseInt(System.getProperty("elapsedScale"))),
+	CPU("Cpu Time", "Cpu Time (ms)", false, System.getProperty("CPUScale")==null?100:Integer.parseInt(System.getProperty("CPUScale"))),
 	SQL_TIME("SQL Time", "SQL Time (sec)", false, 9),
 	SQL_COUNT("SQL Count", "SQL Count (cnt)", false, 50),
 	APICALL_TIME("ApiCall Time", "ApiCall Time (sec)", false, 9),
 	APICALL_COUNT("ApiCall Count", "ApiCall Count (cnt)", false, 50),
-	HEAP_USED("Memory Allocation", "Memory Allocation (KB)", false, 5000);
+	HEAP_USED("Memory Allocation", "Memory Allocation (KB)", false, System.getProperty("memoryScale")==null?5000:Integer.parseInt(System.getProperty("memoryScale")));
 	
 	private final String name;
 	private final String desc;
